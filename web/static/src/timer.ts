1a73663a9d9e90858cc58d31e679ca8ff60cc0a1
jcyunmrrr <jsmn303@gmail.com>
2023-05-03 15:32:53 +0800

    Fixed bug of stop and conti btn

--- web/static/src/timer.ts
+++ web/static/src/timer.ts
@@ -2,9 +2,13 @@ function fmtTime(num: number) {return String(num).padStart(2,'0'); }
 
 let timerInterval:number = 0;
 let elapsed:number = 0;
-
+let isStop: boolean = false;
 
 function timer(minutes: number = 25, seconds: number = 0){
+    isStop = false;
+    document.getElementById('btn_stop').innerHTML =
+        `STOP`;
+
     clearInterval(timerInterval);
     elapsed = minutes * 60 + seconds;
     function secondWork(){
@@ -12,15 +16,31 @@ function timer(minutes: number = 25, seconds: number = 0){
         const minutes = Math.trunc(elapsed / 60);
         const seconds = Math.round(elapsed % 60);
         document.getElementById('timer_count').innerHTML =
-            `${ fmtTime(minutes) }:${fmtTime(seconds)}`;
+            `${ fmtTime(minutes) } : ${fmtTime(seconds)}`;
+        document.getElementById('btn_start').innerHTML =
+            `RESTART`;
     }
     timerInterval = setInterval(secondWork, 1000);
 }
 
+function stop_and_conti(): void{
+    if(isStop){
+        isStop = false;
+        continue_timer();
+        document.getElementById('btn_stop').innerHTML =
+            `STOP`;
+    }
+    else{
+        isStop = true;
+        stop_timer();
+        document.getElementById('btn_stop').innerHTML =
+            `CONTI`;
+    }
+}
 function stop_timer(){
     clearInterval(timerInterval);
 }
 
 function continue_timer(){
-    timer(Math.round(elapsed / 60), Math.round(elapsed % 60));
+    timer(Math.trunc(elapsed / 60), Math.round(elapsed % 60));
 }
