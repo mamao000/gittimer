24e4c9d9ae025588c91869b0411fa5af8bd9bc9c
YiChen Liu <ycl861945@gmail.com>
2023-05-09 06:35:05 +0000

    add get_data route to read GitLab data on web page

--- web/app.py
+++ web/app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import render_template
+from utils.log_tool import read_project
 
 app = Flask(__name__)
 
@@ -14,3 +15,9 @@ def hello():
 @app.route("/hey")
 def hey():
     return "hey"
+
+@app.route('/get_data/<project_id>/<token>', methods=['GET'])
+def get_data(project_id, token):
+    gitlab_res = read_project(project_id, token)
+    return gitlab_res
+
