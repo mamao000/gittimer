3d59bd129f3f8329be15413f19596f9185c38ab9
YiChen Liu <ycl861945@gmail.com>
2023-05-09 08:32:09 +0000

    integrate push_log app.py

--- web/app.py
+++ web/app.py
@@ -1,6 +1,8 @@
 from flask import Flask
-from flask import render_template
-from utils.log_tool import read_project
+from flask import render_template 
+from flask import request
+from utils.log_tool import read_project, push_log
+
 
 app = Flask(__name__)
 
@@ -20,4 +22,12 @@ def hey():
 def get_data(project_id, token):
     gitlab_res = read_project(project_id, token)
     return gitlab_res
+
+
+@app.route('/update_log/<project_id>/<token>', methods=['GET', "POST"])
+def update_log(project_id, token):
+    request_content = request.json
+    print(type(request_content))
+    # push_log(request_content, project_id, token)
+    return request_content
 
