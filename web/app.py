1e20526f842131b5faf6668f8485f3ca818070f2
YiChen Liu <ycl861945@gmail.com>
2023-05-10 05:39:30 +0000

    solve CORS issue on GitLab tool

--- web/app.py
+++ web/app.py
@@ -2,9 +2,11 @@ from flask import Flask
 from flask import render_template 
 from flask import request
 from utils.log_tool import read_project, push_log
+from flask_cors import CORS
 
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/")
 def index():
