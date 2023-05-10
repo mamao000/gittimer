4112782d321aec02e80b5386a89ea2da4cc1f19c
YiChen Liu <ycl861945@gmail.com>
2023-05-10 06:49:11 +0000

    Replace button.js

--- web/static/src/button.js
+++ web/static/src/button.js
@@ -8,15 +8,6 @@ function display_settimerwin(){
             el.style.visibility = 'visible'
 }
 
-function send_GL_Request(Porject_id, access_token) {
-    // let url = "http://localhost:5000?proj_id=" + Porject_id;
-    let url_2 = "http://localhost:5000/get_data/" + Porject_id +"/" + access_token;
-    console.log(url_2)
-    res_data = fetch(url_2)
-    .then(response => response.text())
-    .then(data => alert(data))
-    .catch(error => console.log(error)); 
-    // console.log(res_data);
-    return res_data
-  }
+
+
 
