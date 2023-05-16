ed8793f4085a830704d201630e113b2f2c8a42b1
jcyunmrrr <jsmn303@gmail.com>
2023-05-16 15:10:05 +0800

    Added windows for timer setting & gitlab request

--- web/static/src/button.js
+++ web/static/src/button.js
@@ -1,16 +1,16 @@
-function close_settimerwin(){
-                el = document.querySelector('.win_settimer');
+function close_win(win_id){
+                el = document.querySelector(win_id);
                 el.style.visibility = 'hidden'
 }
 
-function display_settimerwin(){
-            el = document.querySelector('.win_settimer');
+function display_win(win_id){
+            el = document.querySelector(win_id);
             el.style.visibility = 'visible'
 }
 
-function send_GL_Request(Porject_id, access_token) {
-  // let url = "http://localhost:5000?proj_id=" + Porject_id;
-  let url = "http://localhost:5000/get_data/" + Porject_id +"/" + access_token;
+function send_GL_Request(Project_id, access_token) {
+  // let url = "http://localhost:5000?proj_id=" + Project_id;
+  let url = "http://localhost:5000/get_data/" + Project_id +"/" + access_token;
   // console.log(url_2)
   return fetch(url)
     .then(response => response.json())
